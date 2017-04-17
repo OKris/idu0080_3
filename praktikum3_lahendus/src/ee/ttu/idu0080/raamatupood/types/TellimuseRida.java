cd2ae69875deec704681c57deb1effa6e0f52a73
Kristina Oim <kristina.oim@catapultlabs.eu>
2017-04-17 16:15:27 +0300

    ülesanne 3

---
+++ praktikum3_lahendus/src/ee/ttu/idu0080/raamatupood/types/TellimuseRida.java
@@ -0,0 +1,34 @@
+package ee.ttu.idu0080.raamatupood.types;
+
+import java.io.Serializable;
+
+public class TellimuseRida implements Serializable {
+	private Toode toode;
+	private long kogus;
+	
+	public TellimuseRida(Toode toode, long kogus) {
+		this.toode = toode;
+		this.kogus = kogus;
+	}
+	
+	public Toode getToode() {
+		return toode;
+	}
+
+	public void setToode(Toode toode) {
+		this.toode = toode;
+	}
+
+	public long getKogus() {
+		return kogus;
+	}
+
+	public void setKogus(long kogus) {
+		this.kogus = kogus;
+	}
+	
+	@Override
+	public String toString() {
+		return "TellimuseRida: toode=" + toode + ", kogus=" + kogus;
+	}
+}
---
+++ praktikum3_lahendus/src/ee/ttu/idu0080/raamatupood/types/Toode.java
@@ -0,0 +1,37 @@
+package ee.ttu.idu0080.raamatupood.types;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+public class Toode implements Serializable {
+	private Integer kood;
+	private String nimetus;
+	private BigDecimal hind;
+
+	public Toode(int kood, String nimetus, BigDecimal hind) {
+		this.kood = kood;
+		this.nimetus = nimetus;
+		this.hind = hind;
+	}
+	
+	public Integer getKood() {
+		return kood;
+	}
+	
+	public void setKood(Integer kood) {
+		this.kood = kood;
+	}
+	
+	public BigDecimal getHind() {
+		return hind;
+	}
+
+	public void setHind(BigDecimal hind) {
+		this.hind = hind;
+	}
+	
+	@Override
+	public String toString() {
+		return "Toode: kood=" + kood + ", nimetus=" + nimetus + ", hind=" + hind;
+	}
+}
